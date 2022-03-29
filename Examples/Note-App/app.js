dba80a0a75cc1a447a52aec756ebc8cf376ece5c
lhmuana <edd_vai@yahoo.co.in>
2022-03-29 20:20:39 +0530

    va;idator module added

--- Examples/Note-App/app.js
+++ Examples/Note-App/app.js
@@ -1,6 +1,8 @@
-
+const validator = require('validator')
 const  getNotes = require('./notes.js') // Here display can have a name different from the one in utils.js.
 
 const msg = getNotes()
 
 console.log(msg)
+console.log(validator.isEmail('andrew@gmail.com'))
+console.log(validator.isURL('https://stackoverflow.com/questions/45559269/display-flex-creating-extra-space-why'))
