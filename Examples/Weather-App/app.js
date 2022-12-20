dd10378f413d8ed323d57d64d91e6346e66c0bda
lhmuana <edd_vai@yahoo.co.in>
2022-12-20 13:26:35 +0530

    weather app development started

---
+++ Examples/Weather-App/app.js
@@ -0,0 +1,12 @@
+console.log('starting server...');
+
+setTimeout(() => {
+ console.log(' Set the timeout to 700 ms  ');  
+}, 700);
+
+
+setTimeout(() => {
+    console.log('Set the timeout to 1 ms');
+}, 1);
+
+console.log('stopping server...');
