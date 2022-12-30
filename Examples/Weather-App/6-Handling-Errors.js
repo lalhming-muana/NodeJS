ea1d9475fd958c448c9607f35c517dc306fd8c9a
lhmuana <edd_vai@yahoo.co.in>
2022-12-30 21:40:05 +0530

    handling error code created

--- Examples/Weather-App/6-Handling-Errors.js
+++ Examples/Weather-App/6-Handling-Errors.js
@@ -1,1 +1,18 @@
-//this file is for handling errors
+//This file is for handling errors
+
+const request = require('request')
+
+const geourl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGFsaG1pbmciLCJhIjoiY2xjOTJmc2tkMWxyeDN2cDZ1eHAwYjVmbyJ9.IEHEi1VIVOjB9Qy1AttvvA'
+
+request({url: geourl, json: true}, (error,request)=>{
+
+    if(error){
+        console.log('Unable to connect to mapbox.com \nplease check your internet connection');
+    }
+    else{
+        console.log(request.body.features[0].geometry.coordinates);
+    }
+  
+}
+
+)
