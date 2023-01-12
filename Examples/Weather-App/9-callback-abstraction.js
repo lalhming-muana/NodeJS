264d205bb0231c18216f9b3e8e269ad3bc9ad7e7
Muana <lalhming.muana1982@gmail.com>
2023-01-12 20:40:34 +0530

    callback abstraction not complete

--- Examples/Weather-App/9-callback-abstraction.js
+++ Examples/Weather-App/9-callback-abstraction.js
@@ -1,8 +1,32 @@
 /** 
 In Node.js, a callback is a function that is passed as an argument to another function and is executed after the first function completes. The callback abstraction allows for non-blocking, event-driven programming, which is well-suited to building highly concurrent applications. This is because a callback function is called only after an event has occurred, such as a file being read or a network request completing, rather than waiting for the event to occur before continuing execution. This approach allows Node.js to handle multiple events simultaneously and improves the performance of the application.
-
+*
 */
 
+const request = require("request");
+
+
+
 const geocode =(address, callback)=>{
+    const geourl= 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?access_token=pk.eyJ1IjoibGFsaG1pbmciLCJhIjoiY2xjOTJmc2tkMWxyeDN2cDZ1eHAwYjVmbyJ9.IEHEi1VIVOjB9Qy1AttvvA'
+
+    request({url: geourl, json: true}, (error, response)=>{
+
+        if (error){
+             callback('Unable to connect to location services', undefined);      
+        }
+
+        else if(response.body.features.length === 0){
+            callback('unable to find location. Search for another place. ',undefined);
+        }
+
+    })
     
 }
+
+
+geocode('Philadelphia', (error, data)=>{
+    console.log('Error: ',  error);
+    console.log('Data: ', data);
+})
+
--- Examples/Weather-App/app.js
+++ Examples/Weather-App/app.js
@@ -1,3 +1,9 @@
 setTimeout(() => {
     console.log('post check from server after every 800ms....');
-}, 800);
+}, 2000);
+
+const names= ['Andrew','Jen','Jess'];
+
+const shortNames = names.filter(()=>{
+    console.log(names.length<=4);
+})
