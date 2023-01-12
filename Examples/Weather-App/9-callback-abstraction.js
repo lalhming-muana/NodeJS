51f796e9eb281caba150e1c67c5a927ee77d2e43
lhmuana <edd_vai@yahoo.co.in>
2023-01-12 19:58:18 +0530

    callback abstraction file started

---
+++ Examples/Weather-App/9-callback-abstraction.js
@@ -0,0 +1,8 @@
+/** 
+In Node.js, a callback is a function that is passed as an argument to another function and is executed after the first function completes. The callback abstraction allows for non-blocking, event-driven programming, which is well-suited to building highly concurrent applications. This is because a callback function is called only after an event has occurred, such as a file being read or a network request completing, rather than waiting for the event to occur before continuing execution. This approach allows Node.js to handle multiple events simultaneously and improves the performance of the application.
+
+*/
+
+const geocode =(address, callback)=>{
+    
+}
